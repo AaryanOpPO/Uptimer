e6a487792f0baa5754c6c6105ea1d2db84589cc6
DevelopersZoneOfficial <92244831+DevelopersZoneOfficial@users.noreply.github.com>
2021-11-24 19:43:53 +0530

    Update remove.js

--- commands/Uptime/remove.js
+++ commands/Uptime/remove.js
@@ -277,11 +277,10 @@ function sliceIntoChunks(arr, chunkSize) {
 }
 
 
-
 /**
  * @INFO
- * Bot Coded by RogmitOp#6051 |
- * https://www.youtube.com/channel/UCPJRB-I9FCkWzgN3c2vKIpQ
+ * Bot Coded by Crazy |
+ * https://youtube.com/DevelopersZone
  * @INFO
  * Please mention Him , when using this Code!
  * @INFO
